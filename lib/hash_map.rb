12f9de9c718c8ffed2a8f1edfdb2721f8f763ba8
lbackman <lucasbackman@gmail.com>
2024-02-05 15:10:46 +0200

    Add method for growing the table

--- lib/hash_map.rb
+++ lib/hash_map.rb
@@ -4,7 +4,6 @@ require_relative 'linked_list'
 
 class HashMap
   LOAD_FACTOR = 0.75
-  attr_reader :hash, :buckets_occupied
 
   def initialize
     set_up
@@ -12,46 +11,47 @@ class HashMap
 
   def set(key, value)
     # sets key value pair
-    # increase bucket amount if necessary
     index = hash_index(key)
-    bucket = hash[index]
-    if bucket.head.nil?
-      @buckets_occupied += 1
-    end
-    # set key-value pair
+    bucket = @hash[index]
     bucket.add(key, value)
-    if buckets_occupied.to_f / hash.size > LOAD_FACTOR
-      # grow_hash_table
+    @key_value_pairs += 1
+    # increase bucket amount if necessary
+    if @key_value_pairs.to_f / @hash.size > LOAD_FACTOR
+      grow_hash_table
     end
+    value
   end
+  alias_method :[]=, :set
 
   def get(key)
     #returns value associated with key, if not found returns nil
     index = hash_index(key)
     raise IndexError if index.negative? || index >= @hash.size
 
-    bucket = hash[index]
+    bucket = @hash[index]
     return nil if bucket.head.nil?
 
     bucket.get(key)
   end
+  alias_method :[], :get
 
   def key?(key)
     # return true/false depending on if key is in the hash map
     index = hash_index(key)
-    hash[index].contains?(key)
+    @hash[index].contains?(key)
   end
 
   def remove(key)
     # removes k-v pair and returns the value
     # if key not in hash, return nil
     index = hash_index(key)
-    hash[index].remove(key)
+    @key_value_pairs -= 1 if @hash[index].contains?(key)
+    @hash[index].remove(key)
   end
 
   def length
     # amount of keys in hash
-    @hash.map { |bucket| bucket.size }.sum
+    @key_value_pairs
   end
 
   def clear
@@ -88,11 +88,20 @@ class HashMap
     entries
   end
 
+  def to_s
+    s = []
+    entries.each do |entry|
+      s << entry.join(' => ')
+    end
+    "{#{s.join(', ')}}"
+  end
+
   private
 
-  def set_up
-    @hash = Array.new(16) { LinkedList.new }
-    @buckets_occupied = 0
+  def set_up(bucket_amount = 16)
+    @bucket_amount = bucket_amount
+    @hash = Array.new(bucket_amount) { LinkedList.new }
+    @key_value_pairs = 0
   end
 
   def hash_code(key)
@@ -110,5 +119,11 @@ class HashMap
   def hash_index(key)
     # If the table has size 2^n, we can use "& (length - 1)" instead "% length"
     hash_code(key) & hash.size - 1
+  end
+
+  def grow_hash_table
+    temp_entries = entries
+    set_up(@bucket_amount * 2)
+    temp_entries.each { |key, val| set(key, val) }
   end
 end
